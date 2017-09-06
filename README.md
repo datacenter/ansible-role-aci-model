a96e2ca5a1a6cbd1d7274f9a970eec94aa88c064
Ramses Smeyers <rsmeyers@cisco.com>
2017-09-06 02:35:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# aci-model
-Repo for roles to be hosted on Ansible Galaxy. Use in conjunction with Ansible
+# Ansible Role: aci_model
+
+A comprehensive Ansible role to model and deploy Cisco ACI fabrics
+
+## Requirements
+
+This role requires the **aci_rest** module and a standard set of ACI modules from Ansible v2.4.
+
+
+## Role variables
+
+Available variables are listed below, along with default values:
+
+    aci_model_data:
+      
+## Example playbook
+
+    - hosts: *apic1
+      roles:
+      - role: aci_model
+        aci_model_data: '{{ your.inventory.aci_topology }}'
+
+## License
+GPLv3
---
+++ plugins/filter/aci.py
@@ -0,0 +1,39 @@
+# Copyright: (c) 2017, Ramses Smeyers <rsmeyers@cisco.com>
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+def listify(d, *keys):
+    return listify_worker(d, keys, 0, [], {}, '')
+
+def listify_worker(d, keys, depth, result, cache, prefix):
+    prefix += keys[depth] + '_'
+
+    for item in d[keys[depth]]:
+        cache_work = cache.copy()
+        if isinstance(item, dict):
+            for k,v in item.iteritems():
+                if not isinstance(v, dict) and not isinstance(v, list):
+                    cache_key = prefix + k
+                    cache_value = v
+                    cache_work[cache_key] = cache_value
+
+            if len(keys)-1 == depth :
+                result.append(cache_work)
+            else:
+                for k,v in item.iteritems():
+                    if k == keys[depth+1]:
+                        if isinstance(v, dict) or isinstance(v, list):
+                            result = listify_worker({k:v}, keys, depth+1, result, cache_work, prefix)
+
+    return result
+
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'aci_listify': listify,
+        }
